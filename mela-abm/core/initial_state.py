c03299109be2c34669791f7704bf3aa6d09cdc75
Nathan Schofield <nathan.schofield@dtc.ox.ac.uk>
2023-10-18 12:29:34 +0100

    Runner saves datafrme at each time to a single csv file to be used for plotting

--- mela-abm/core/initial_state.py
+++ mela-abm/core/initial_state.py
@@ -5,37 +5,39 @@ import random
 from cell import Cell
 from domain import Domain
 
-avaliable_spaces = Domain.create_uniform_2D_grid(0, 10, 11, 0, 10, 11)
-num_space = np.shape(avaliable_spaces)
-# print('Domain', avaliable_spaces)
-print('Number of spaces', num_space[1])
+def initialise_2D(initial_number_cells, xmin,xmax,ymin,ymax):
 
-initial_number_cells = 117
+    avaliable_spaces = Domain.create_uniform_2D_grid(xmin, xmax, ymin, ymax)
+    num_space = np.shape(avaliable_spaces)
+    # print('Domain', avaliable_spaces)
+    print('Number of spaces', num_space[1])
 
-cols = ['Phenotype', 'Location']
-cells = pd.DataFrame(columns=cols)
+    # initial_number_cells = 117
 
-cell_list = []
-cell_initial_locs = [[0,0]]
-cell_phenotypes = []
-for i in range(initial_number_cells):
-    # creating list
-    chance_of_invasive = 0.5
-    new_cell = Cell(False,[0,0])
-    avaliable_spaces, selected_location = new_cell.set_initial_location(avaliable_spaces)
-    phenotype = new_cell.set_initial_phenotype(chance_of_invasive)
+    cols = ['Phenotype', 'Location']
+    cells = pd.DataFrame(columns=cols)
 
-    cell_phenotypes = np.append(cell_phenotypes, phenotype)
-    # appending instances to list
-    cell_list.append(Cell(phenotype, selected_location))
-    print(selected_location)
-    if i == 0:
-        cell_initial_locs = selected_location
-    else:
-        cell_initial_locs = np.append(cell_initial_locs, selected_location, axis=0)
+    cell_list = []
+    cell_initial_locs = [[0,0]]
+    cell_phenotypes = []
+    for i in range(initial_number_cells):
+        # creating list
+        chance_of_invasive = 0.5
+        new_cell = Cell(False,[0,0])
+        avaliable_spaces, selected_location = new_cell.set_initial_location(avaliable_spaces)
+        phenotype = new_cell.set_initial_phenotype(chance_of_invasive)
+
+        cell_phenotypes = np.append(cell_phenotypes, phenotype)
+        # appending instances to list
+        cell_list.append(Cell(phenotype, selected_location))
+        print(selected_location)
+        if i == 0:
+            cell_initial_locs = selected_location
+        else:
+            cell_initial_locs = np.append(cell_initial_locs, selected_location, axis=0)
 
 
-list_of_tuples = list(zip(cell_phenotypes, cell_initial_locs))
-cells_after = pd.DataFrame(list_of_tuples, columns=cols)
-cells_after
-cells_after.to_csv('output1.csv', index=True, header=True)
+    list_of_tuples = list(zip(cell_phenotypes, cell_initial_locs))
+    cells_after = pd.DataFrame(list_of_tuples, columns=cols)
+    cells_after
+    cells_after.to_csv('initial_2D.csv', index=True, header=True)
--- mela-abm/core/master_runner.py
+++ mela-abm/core/master_runner.py
@@ -2,6 +2,58 @@
 Main script to run
 '''
 
+import numpy as np
+import pandas as pd
+
+import initial_state
+
+## Parameters to change
+xmin = 0
+xmax = 10
+ymin = 0
+ymax = 10
+initial_number_cells = 90
+timesteps = 3
+
 ## Initialise
 
+initial_state.initialise_2D(initial_number_cells, xmin,xmax,ymin,ymax)
+
 ## Loop over event selector for each cell at each timestep
+
+cells_current = pd.read_csv('initial_2D.csv')  
+
+# Declare a list that is to be converted into a column
+count_row = cells_current.shape[0]
+
+timestep_current = 0
+
+current_time = np.full((count_row,1), timestep_current)
+ 
+# Using 'Address' as the column name
+# and equating it to the list
+cells_current['Time'] = current_time
+
+cells_current
+
+cell_output = cells_current
+
+
+## Main loop
+for i in range (timesteps):
+    timestep_current += 1
+
+    ## Shuffle the dataframe in order to get random order to cycle over cells
+    cells_current = cells_current.sample(frac = 1)
+
+    ## Apply changes calls here
+
+
+    current_time = np.full((count_row,1), timestep_current)
+    cells_current['Time'] = current_time
+    # cell_output.concat(cells_current)
+    cell_output = pd.concat([cell_output, cells_current])
+
+# cell_output.append(cells_current)
+
+cell_output.to_csv('output_2D.csv', index=True, header=True)
